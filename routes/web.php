a16d820e06734501fe0f6da18afa18ed652e7ed1
aryaddinata <aryaddinataabbas017@gmail.com>
2024-10-23 05:04:12 +0900

    fix error

--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,10 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\BerandaController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
+Route::get('/', [BerandaController::class, 'index'])->name('index');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
@@ -14,5 +12,5 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-require __DIR__.'/auth.php';
-require __DIR__.'/backend.php';
+require __DIR__ . '/auth.php';
+require __DIR__ . '/backend.php';
--- tailwind.config.js
+++ tailwind.config.js
@@ -12,10 +12,23 @@ export default {
     theme: {
         extend: {
             fontFamily: {
-                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+                sans: [ 'Figtree', ...defaultTheme.fontFamily.sans ],
             },
         },
     },
 
-    plugins: [forms],
+    plugins: [ forms ],
 };
+
+// tailwind.config.js
+module.exports = {
+    content: [
+        './src/**/*.{js,jsx,ts,tsx,html}', // Sesuaikan path sesuai proyek Anda
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [
+        require( '@tailwindcss/forms' ), // Pastikan plugin ini sudah ditambahkan
+    ],
+}
