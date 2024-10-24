786abf365050adc8c646b9598dda2cfde10948c4
aryaddinata <aryaddinataabbas017@gmail.com>
2024-10-25 03:04:02 +0900

    fix daftar peserta

--- resources/views/frontend/pages/data-peserta.blade.php
+++ resources/views/frontend/pages/data-peserta.blade.php
@@ -11,7 +11,7 @@
             <div class="p-6">
                 <!-- Table with striped rows -->
                 <div class="overflow-x-auto">
-                    <table class=" min-w-full bg-white text-center border border-gray-300 rounded-lg">
+                    <table class="min-w-full bg-white text-center border border-gray-300 rounded-lg">
                         <thead class="bg-gray-200">
                             <tr>
                                 <th class="py-2 px-4 border-b border-gray-300">Nama Lengkap</th>
@@ -25,7 +25,13 @@
                             <!-- Looping data dari controller -->
                             <tr class="hover:bg-gray-100">
                                 <td class="py-2 px-4 border-b border-gray-300">{{ $peserta->nama_lengkap }}</td>
-                                <td class="py-2 px-4 border-b border-gray-300">{{ $peserta->no_tlp }}</td>
+                                <td class="py-2 px-4 border-b border-gray-300">
+                                    @php
+                                    $maskedPhone = str_repeat('*', strlen($peserta->no_tlp) - 4) .
+                                    substr($peserta->no_tlp, -4);
+                                    @endphp
+                                    {{ $maskedPhone }}
+                                </td>
                                 <td class="py-2 px-4 border-b border-gray-300">{{ $peserta->size_jersey }}</td>
                                 <td class="py-2 px-4 border-b border-gray-300">
                                     <a class="inline-block px-3 py-1 rounded-lg text-white {{ $peserta->status == 'terverifikasi' ? 'bg-green-500' : 'bg-red-500' }}"
@@ -51,6 +57,4 @@
 @section('js')
 <!-- jQuery CDN -->
 <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-
-
 @endsection
