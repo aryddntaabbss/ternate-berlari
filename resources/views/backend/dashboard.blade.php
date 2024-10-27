4a5a3a32a0c39cc812fa6907e9df0dc5515d8eda
wahyuumaternate <retmujago@gmail.com>
2024-10-27 23:16:43 +0900

    js

--- resources/views/backend/dashboard.blade.php
+++ resources/views/backend/dashboard.blade.php
@@ -21,52 +21,8 @@
                     <!-- Pie Chart -->
                     <div id="pieChart" style="height: 400px;"></div>
 
-                    <script src="https://cdn.jsdelivr.net/npm/echarts/dist/echarts.min.js"></script>
-                    <script>
-                        document.addEventListener("DOMContentLoaded", () => {
-                            const data = @json($data); // Data dari controller
 
-                            console.log(data); // Debugging: Pastikan data muncul di konsol
 
-                            // Siapkan data untuk ECharts
-                            const seriesData = data.map(item => ({
-                                name: 'Kategori ' + item.nama_road_race + 'K',
-                                value: item.total
-                            }));
-
-                            // Inisialisasi ECharts
-                            const chartDom = document.getElementById('pieChart');
-                            const myChart = echarts.init(chartDom);
-
-                            const option = {
-
-                                tooltip: {
-                                    trigger: 'item',
-                                    formatter: '{a} <br/>{b}: {c} ({d}%)'
-                                },
-                                legend: {
-                                    orient: 'vertical',
-                                    left: 'left',
-                                },
-                                series: [{
-                                    name: 'Peserta',
-                                    type: 'pie',
-                                    radius: '50%',
-                                    data: seriesData,
-                                    emphasis: {
-                                        itemStyle: {
-                                            shadowBlur: 10,
-                                            shadowOffsetX: 0,
-                                            shadowColor: 'rgba(0, 0, 0, 0.5)'
-                                        }
-                                    }
-                                }]
-                            };
-
-                            // Render chart
-                            myChart.setOption(option);
-                        });
-                    </script>
                     <!-- End Pie Chart -->
                 </div>
             </div>
@@ -81,56 +37,104 @@
                     <!-- Pie Chart -->
                     <div id="chart-2" style="height: 400px;"></div>
 
-                    <script src="https://cdn.jsdelivr.net/npm/echarts/dist/echarts.min.js"></script>
-                    <script>
-                        document.addEventListener("DOMContentLoaded", () => {
-                            const data = @json($data2); // Data dari controller
 
-                            console.log(data); // Debugging: pastikan data muncul di konsol
-
-                            // Siapkan data untuk ECharts
-                            const seriesData = data.map(item => ({
-                                name: 'Kategori ' + item.name_kategori,
-                                value: item.total
-                            }));
-
-                            // Inisialisasi ECharts
-                            const chartDom = document.getElementById('chart-2');
-                            const myChart = echarts.init(chartDom);
-
-                            const option = {
-
-                                tooltip: {
-                                    trigger: 'item',
-                                    formatter: '{a} <br/>{b}: {c} ({d}%)'
-                                },
-                                legend: {
-                                    orient: 'vertical',
-                                    left: 'left',
-                                },
-                                series: [{
-                                    name: 'Peserta',
-                                    type: 'pie',
-                                    radius: '50%',
-                                    data: seriesData,
-                                    emphasis: {
-                                        itemStyle: {
-                                            shadowBlur: 10,
-                                            shadowOffsetX: 0,
-                                            shadowColor: 'rgba(0, 0, 0, 0.5)'
-                                        }
-                                    }
-                                }]
-                            };
-
-                            // Render chart
-                            myChart.setOption(option);
-                        });
-                    </script>
-                    <!-- End Pie Chart -->
                 </div>
             </div>
         </div>
         <!-- End Reports -->
     </div>
+@endsection
+@section('js')
+    <script src="https://cdn.jsdelivr.net/npm/echarts/dist/echarts.min.js"></script>
+    <script>
+        document.addEventListener("DOMContentLoaded", () => {
+            const data = @json($data); // Data dari controller
+
+            console.log(data); // Debugging: Pastikan data muncul di konsol
+
+            // Siapkan data untuk ECharts
+            const seriesData = data.map(item => ({
+                name: 'Kategori ' + item.nama_road_race + 'K',
+                value: item.total
+            }));
+
+            // Inisialisasi ECharts
+            const chartDom = document.getElementById('pieChart');
+            const myChart = echarts.init(chartDom);
+
+            const option = {
+
+                tooltip: {
+                    trigger: 'item',
+                    formatter: '{a} <br/>{b}: {c} ({d}%)'
+                },
+                legend: {
+                    orient: 'vertical',
+                    left: 'left',
+                },
+                series: [{
+                    name: 'Peserta',
+                    type: 'pie',
+                    radius: '50%',
+                    data: seriesData,
+                    emphasis: {
+                        itemStyle: {
+                            shadowBlur: 10,
+                            shadowOffsetX: 0,
+                            shadowColor: 'rgba(0, 0, 0, 0.5)'
+                        }
+                    }
+                }]
+            };
+
+            // Render chart
+            myChart.setOption(option);
+        });
+    </script>
+    <script>
+        document.addEventListener("DOMContentLoaded", () => {
+            const data = @json($data2); // Data dari controller
+
+            console.log(data); // Debugging: pastikan data muncul di konsol
+
+            // Siapkan data untuk ECharts
+            const seriesData = data.map(item => ({
+                name: 'Kategori ' + item.name_kategori,
+                value: item.total
+            }));
+
+            // Inisialisasi ECharts
+            const chartDom = document.getElementById('chart-2');
+            const myChart = echarts.init(chartDom);
+
+            const option = {
+
+                tooltip: {
+                    trigger: 'item',
+                    formatter: '{a} <br/>{b}: {c} ({d}%)'
+                },
+                legend: {
+                    orient: 'vertical',
+                    left: 'left',
+                },
+                series: [{
+                    name: 'Peserta',
+                    type: 'pie',
+                    radius: '50%',
+                    data: seriesData,
+                    emphasis: {
+                        itemStyle: {
+                            shadowBlur: 10,
+                            shadowOffsetX: 0,
+                            shadowColor: 'rgba(0, 0, 0, 0.5)'
+                        }
+                    }
+                }]
+            };
+
+            // Render chart
+            myChart.setOption(option);
+        });
+    </script>
+    <!-- End Pie Chart -->
 @endsection
