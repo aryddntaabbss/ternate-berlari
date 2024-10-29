f10af178938fcd0f1e628b481a5034fc8a3e151f
aryaddinata <aryaddinataabbas017@gmail.com>
2024-10-30 01:35:51 +0900

    fix data peserta

--- resources/views/frontend/layouts/main.blade.php
+++ resources/views/frontend/layouts/main.blade.php
@@ -28,6 +28,19 @@
         @vite(['resources/css/app.css','resources/js/app.js'])
 
         @notifyCss
+
+        <!-- Vendor CSS Files -->
+        <link href="{{ asset('backend/vendor/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet">
+        <link href="{{ asset('backend/vendor/bootstrap-icons/bootstrap-icons.css') }}" rel="stylesheet">
+        <link href="{{ asset('backend/vendor/boxicons/css/boxicons.min.css') }}" rel="stylesheet">
+        <link href="{{ asset('backend/vendor/quill/quill.snow.css') }}" rel="stylesheet">
+        <link href="{{ asset('backend/vendor/quill/quill.bubble.css') }}" rel="stylesheet">
+        <link href="{{ asset('backend/vendor/remixicon/remixicon.css') }}" rel="stylesheet">
+        <link href="{{ asset('backend/vendor/simple-datatables/style.css') }}" rel="stylesheet">
+
+        <!-- Template Main CSS File -->
+        <link href="{{ asset('backend/css/style.css') }}" rel="stylesheet">
+
         <style>
             .notify {
                 position: fixed;
@@ -51,6 +64,19 @@
         @include('frontend.layouts.footer')
 
         <script src="https://cdn.tailwindcss.com"></script>
+
+        <!-- Vendor JS Files -->
+        <script src="{{ asset('backend/vendor/apexcharts/apexcharts.min.js') }}"></script>
+        <script src="{{ asset('backend/vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+        <script src="{{ asset('backend/vendor/chart.js/chart.umd.js') }}"></script>
+        <script src="{{ asset('backend/vendor/echarts/echarts.min.js') }}"></script>
+        <script src="{{ asset('backend/vendor/quill/quill.js') }}"></script>
+        <script src="{{ asset('backend/vendor/simple-datatables/simple-datatables.js') }}"></script>
+        <script src="{{ asset('backend/vendor/tinymce/tinymce.min.js') }}"></script>
+        <script src="{{ asset('backend/vendor/php-email-form/validate.js') }}"></script>
+
+        <!-- Template Main JS File -->
+        <script src="{{ asset('backend/js/main.js') }}"></script>
 
         <x-notify::notify />
         @notifyJs
