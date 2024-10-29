44401a0d809ca4ada01ac648a1385177607b7224
aryaddinata <aryaddinataabbas017@gmail.com>
2024-10-30 01:35:20 +0900

    fix data peserta

--- resources/views/frontend/pages/data-peserta.blade.php
+++ resources/views/frontend/pages/data-peserta.blade.php
@@ -1,74 +1,75 @@
 @extends('frontend.layouts.main')
 
 @section('body')
-    @include('frontend.layouts.header')
+@include('frontend.layouts.header')
 
-    <!-- Section untuk Data Peserta -->
-    <section class="section mt-5">
-        <div class="w-full mx-auto px-4">
-            <h2 class="text-2xl font-bold text-gray-800 text-center pb-5" data-aos="fade-up" data-aos-duration="15000">
-                DAFTAR PESERTA</h2>
-            <div class="bg-white shadow-md rounded-lg" data-aos="fade-up" data-aos-duration="15000">
-                <div class="p-6">
-                    <!-- Table with striped rows -->
-                    <div class="overflow-x-auto">
-                        <table class="min-w-full bg-white text-center border border-gray-300 rounded-lg">
-                            <thead class="bg-gray-200">
-                                <tr>
-                                    <th class="py-2 px-4 border-b border-gray-300">Nama Lengkap</th>
-                                    <th class="py-2 px-4 border-b border-gray-300">No Tlp</th>
-                                    <th class="py-2 px-4 border-b border-gray-300">Paket</th>
-                                    <th class="py-2 px-4 border-b border-gray-300">Kategori Usia</th>
-                                    {{-- <th class="py-2 px-4 border-b border-gray-300">Ukuran Jersey</th> --}}
-                                    <th class="py-2 px-4 border-b border-gray-300">Status</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                @foreach ($data_peserta as $peserta)
-                                    <!-- Looping data dari controller -->
-                                    <tr class="hover:bg-gray-100">
-                                        <td class="py-2 px-4 border-b border-gray-300">{{ $peserta->nama_lengkap }}</td>
-                                        <td class="py-2 px-4 border-b border-gray-300">
-                                            @php
-                                                $maskedPhone =
-                                                    str_repeat('*', strlen($peserta->no_tlp) - 4) .
-                                                    substr($peserta->no_tlp, -4);
-                                            @endphp
-                                            {{ $maskedPhone }}
-                                        </td>
-                                        <td class="py-2 px-4 border-b border-gray-300">{{ $peserta->roadRace->nama }} <small
-                                                class="text-gray-500">Km</small></td>
-                                        @if ($peserta->kategori->id != 1)
-                                            <td class="py-2 px-4 border-b border-gray-300">{{ $peserta->kategori->name }}
-                                            </td>
-                                        @else
-                                            <td class="py-2 px-4 border-b border-gray-300">-
-                                            </td>
-                                        @endif
-                                        {{-- <td class="py-2 px-4 border-b border-gray-300">{{ $peserta->size_jersey }}</td>
-                                --}}
-                                        <td class="py-2 px-4 border-b border-gray-300">
-                                            <a class="inline-block px-3 py-1 rounded-lg text-white {{ $peserta->status == 'terverifikasi' ? 'bg-green-500' : 'bg-red-500' }}"
-                                                onclick="confirmStatusChange({{ $peserta->id }}, '{{ $peserta->status }}')">
-                                                {{ ucfirst($peserta->status) }}
-                                            </a>
-                                        </td>
-                                    </tr>
-                                @endforeach
-                            </tbody>
-                        </table>
-                    </div>
-                    <!-- End Table with striped rows -->
+<!-- Section untuk Data Peserta -->
+<section class="section mt-5">
+    <div class="w-full mx-auto px-4">
+        <h2 class="text-2xl font-bold text-gray-800 text-center pb-5" data-aos="fade-up" data-aos-duration="15000">
+            DAFTAR PESERTA</h2>
+        <div class="bg-white shadow-md rounded-lg" data-aos="fade-up" data-aos-duration="15000">
+            <div class="p-6">
+                <!-- Table with striped rows -->
+                <div class="overflow-x-auto">
+                    <table class="table datatable">
+                        <thead>
+                            <tr>
+                                <th>Nama Lengkap</th>
+                                <th>Paket</th>
+                                <th>Kategori Usia</th>
+                                <th>Size Jersey</th>
+                                <th>Status</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach ($data_peserta as $peserta)
+                            <!-- Looping data dari controller -->
+                            <tr>
+                                <td>{{ $peserta->nama_lengkap }}</td>
+                                <td>{{ $peserta->roadRace->nama }} <small>KM</small></td>
+                                @if ($peserta->kategori->id != 1)
+                                <td>{{ $peserta->kategori->name }}</td>
+                                @else
+                                <td>-</td>
+                                @endif
+                                <td>{{ $peserta->size_jersey }}</td>
+                                <td>
+                                    <a href="javascript:void(0)"
+                                        class="btn {{ $peserta->status == 'terverifikasi' ? 'btn-success' : 'btn-danger' }} text-light"
+                                        onclick="confirmStatusChange({{ $peserta->id }}, '{{ $peserta->status }}')">
+                                        {{ $peserta->status }}
+                                    </a>
+                                </td>
+                            </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
                 </div>
+                <!-- End Table with striped rows -->
             </div>
         </div>
-    </section>
+    </div>
+</section>
 
-    <!-- Scroll to Top Button -->
-    @include('frontend.components.to-top')
+<!-- Scroll to Top Button -->
+@include('frontend.components.to-top')
 @endsection
 
 @section('js')
-    <!-- jQuery CDN -->
-    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+<!-- jQuery CDN -->
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+<!-- DataTables CSS and JS -->
+<link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.21/css/jquery.dataTables.css">
+<script src="https://cdn.datatables.net/1.10.21/js/jquery.dataTables.min.js"></script>
+<script>
+    $(document).ready(function() {
+            $('#pesertaTable').DataTable({
+                "order": [], // By default, it will be unsorted; you can set initial sorting here.
+                "language": {
+                    "search": "Cari di tabel:" // Customizes the search field placeholder text.
+                }
+            });
+        });
+</script>
 @endsection
